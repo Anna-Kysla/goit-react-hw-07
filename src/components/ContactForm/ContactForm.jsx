c827b75887ed984b0b102d163bd2deb2c45a607e
Anna K <1415ks@ukr.net>
2025-05-04 20:39:07 +0300

    first

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,54 +1,46 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import css from "./ContactForm.module.css";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { addContact } from "../../redux/contactsSlice";
+import { addContact } from "../../redux/contactsOps";
+import styles from "./ContactForm.module.css";
 
 const ContactForm = () => {
   const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
 
-  const initialValues = { name: "", number: "" };
-
-  const validationSchema = Yup.object({
-    name: Yup.string()
-      .min(3, "Мінімум 3 символи")
-      .max(50, "Максимум 50 символів")
-      .required("Required"),
-    number: Yup.string()
-      .min(3, "Мінімум 3 символи")
-      .max(50, "Максимум 50 символів")
-      .required("Required"),
-  });
-
-  const handleSubmit = (values, actions) => {
-    dispatch(addContact(values));
-    actions.resetForm();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addContact({ name, phone }));
+    setName("");
+    setPhone("");
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      <Form className={css.form}>
-        <label className={css.label}>
-          Name
-          <Field type="text" name="name" className={css.input} />
-          <ErrorMessage name="name" component="div" className={css.error} />
-        </label>
-
-        <label className={css.label}>
-          Number
-          <Field type="text" name="number" className={css.input} />
-          <ErrorMessage name="number" component="div" className={css.error} />
-        </label>
-
-        <button type="submit" className={css.button}>
-          Add contact
-        </button>
-      </Form>
-    </Formik>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label className={styles.label}>
+        Name:
+        <input
+          className={styles.input}
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </label>
+      <label className={styles.label}>
+        Phone:
+        <input
+          className={styles.input}
+          type="tel"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+        />
+      </label>
+      <button className={styles.button} type="submit">
+        Add contact
+      </button>
+    </form>
   );
 };
 
